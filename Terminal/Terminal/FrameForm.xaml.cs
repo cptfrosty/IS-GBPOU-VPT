fddedf58c7793f54b8efc0fc9550a00c2038ed48
Twinkle <dim.iwanencko2014@gmail.com>
2022-04-22 13:11:42 +0400

    MEGA PUSH

--- Terminal/Terminal/FrameForm.xaml.cs
+++ Terminal/Terminal/FrameForm.xaml.cs
@@ -33,55 +33,20 @@ namespace Terminal
         {
             InitializeComponent();
             this.Choice = id;
-        
+
             //PS ссылка на картинку в XML файле должна быть
-            Picture.Source = new BitmapImage(new Uri($"pack://application:,,,/{id}.jpg"));
-            ShowInfo(id);
+            Picture.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + $"Image/Corp/{id}.jpg"));
+
+            PictureMapping.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + $"Image/Corp/{id + 100}.jpg"));
+
+            InitButton(id);
             
         }
 
-        public void ShowInfo(int id)
+        public void InitButton(int id)
         {
-            XDocument xdoc = XDocument.Load(AppDomain.CurrentDomain.BaseDirectory + "Frame/Corps.xml");
-            foreach (XElement informations in xdoc.Element("informations").Elements("info"))
-            {
-                XAttribute nameAttribute = informations.Attribute("name");
-                XElement adressElement = informations.Element("adress");
-                XElement telephoneElement = informations.Element("telephone");
-                XElement emailElement = informations.Element("email");
-                XElement graphicElement = informations.Element("graphic");
-
-                
-
-                switch (id)
-                {
-                    default: break;
-                    case 1:
-                        if(nameAttribute.Value == "Корпус 1")
-                        {
-                            PlaceInfo(nameAttribute, adressElement, telephoneElement, emailElement, graphicElement);
-                        }
-                        break;
-                    case 2:
-                        if (nameAttribute.Value == "Корпус 2")
-                        {
-                            PlaceInfo(nameAttribute, adressElement, telephoneElement, emailElement, graphicElement);
-                        }
-                        break;
-                    case 3:
-                        if (nameAttribute.Value == "Корпус 3")
-                        {
-                            PlaceInfo(nameAttribute, adressElement, telephoneElement, emailElement, graphicElement);
-                        }
-                        break;
-                    case 4:
-                        if (nameAttribute.Value == "Корпус 4")
-                        {
-                            PlaceInfo(nameAttribute, adressElement, telephoneElement, emailElement, graphicElement);
-                        }
-                        break;
-                }
-            }
+            CollegeBuilding currentCorp = XmlCollegeBuilding.Instance().GetCollegeBuilding[id-1];
+            PlaceInfo(currentCorp);
         }
 
         /// <summary>
@@ -92,13 +57,75 @@ namespace Terminal
         /// <param name="telephoneElement"></param>
         /// <param name="emailElement"></param>
         /// <param name="graphicElement"></param>
-        public void PlaceInfo(XAttribute nameAttribute, XElement adressElement, XElement telephoneElement, XElement emailElement, XElement graphicElement)
+        public void PlaceInfo(CollegeBuilding corp)
         {
-            CorpLabel.Content = nameAttribute.Value;
-            AdressLabel.Content = adressElement.Value;
-            TelephoneLabel.Content = telephoneElement.Value;
-            EmailLabel.Content = emailElement.Value;
-            GraphicLabel.Content = graphicElement.Value;
+            CorpLabel.Content = corp.nameAttribute;
+            AdressLabel.Text = corp.adressElement;
+            TelephoneLabel.Content = corp.telephoneElement;
+            EmailLabel.Content = corp.emailElement;
+            GraphicLabel.Content = corp.workSchedule;
+            HistoryLabel.Text = corp.history;
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        //TEST
+        private System.Windows.Point scrollTarget;
+        private System.Windows.Point scrollStartPoint;
+        private System.Windows.Point scrollStartOffset;
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            if (scrollViewer.IsMouseOver)
+            {
+                // Save starting point, used later when determining how much to scroll.
+                scrollStartPoint = e.GetPosition(this);
+                scrollStartOffset.X = scrollViewer.HorizontalOffset;
+                scrollStartOffset.Y = scrollViewer.VerticalOffset;
+
+                // Update the cursor if can scroll or not.
+                this.Cursor = (scrollViewer.ExtentWidth > scrollViewer.ViewportWidth) ||
+                    (scrollViewer.ExtentHeight > scrollViewer.ViewportHeight) ?
+                    Cursors.ScrollAll : Cursors.Arrow;
+
+                this.CaptureMouse();
+            }
+
+            base.OnPreviewMouseDown(e);
+        }
+
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            if (this.IsMouseCaptured)
+            {
+                System.Windows.Point currentPoint = e.GetPosition(this);
+
+                // Determine the new amount to scroll.
+                System.Windows.Point delta = new System.Windows.Point(scrollStartPoint.X -
+                    currentPoint.X, scrollStartPoint.Y - currentPoint.Y);
+
+                scrollTarget.X = scrollStartOffset.X + delta.X;
+                scrollTarget.Y = scrollStartOffset.Y + delta.Y;
+
+                // Scroll to the new position.
+                scrollViewer.ScrollToHorizontalOffset(scrollTarget.X);
+                scrollViewer.ScrollToVerticalOffset(scrollTarget.Y);
+            }
+
+            base.OnPreviewMouseMove(e);
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            if (this.IsMouseCaptured)
+            {
+                this.Cursor = Cursors.Arrow;
+                this.ReleaseMouseCapture();
+            }
+
+            base.OnPreviewMouseUp(e);
         }
     }
 }
--- Terminal/Terminal/MainWindow.xaml.cs
+++ Terminal/Terminal/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using CallOpenWeather;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Terminal.Weather;
+using Terminal.OpenWeather;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Sockets;
 
 namespace Terminal
 {
@@ -24,11 +31,24 @@ namespace Terminal
         {
             InitializeComponent();
 
+            CheckNetwork();
+
+            Weather();
+
+
+            //Время
             var timer = new System.Windows.Threading.DispatcherTimer();
             timer.Interval = new TimeSpan(0, 0, 1);
             timer.IsEnabled = true;
             timer.Tick += (o, t) => { Time.Content = DateTime.Now.ToShortTimeString(); };
             timer.Start();
+
+            //Обновление погоды
+            var timerWeather = new System.Windows.Threading.DispatcherTimer();
+            timerWeather.Interval = new TimeSpan(0, 0, 5);
+            timerWeather.IsEnabled = true;
+            timerWeather.Tick += (o, t) => { Weather(); };
+            timerWeather.Start();
         }
 
         private void MainInformation(object sender, RoutedEventArgs e)
@@ -41,6 +61,94 @@ namespace Terminal
 
             WindowInf windowInf = new WindowInf();
             windowInf.ShowDialog();
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MuseumWin museumWin = new MuseumWin();
+            museumWin.ShowDialog();
+        }
+
+        //Погода
+        private async void Weather()
+        {
+            if (CheckNetwork())
+            {
+                
+
+                string city = "Волжский";
+                WebRequest request = null;
+                WebResponse response = await Call.CallWeather(city, request).GetResponseAsync();
+
+                string answer = string.Empty;
+
+                using (Stream s = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(s))
+                    {
+                        answer = await reader.ReadToEndAsync();
+                    }
+                }
+
+                response.Close();
+
+                OpenWeather.OpenWeather oW = JsonConvert.DeserializeObject<OpenWeather.OpenWeather>(answer);
+
+                //string icon = oW.weather[0].icon;
+
+                LabelTemp.Content = oW.main.temp.ToString("#") + "°";
+
+                Translate translate = new Translate(oW.weather[0].main);
+
+                LabelMain.Content = translate.GetWord();
+
+                LabelCity.Content = city;
+
+                Text_Pressure.Content = ((int)oW.main.pressure).ToString() + "мм";
+
+                Text_Humidity.Content = oW.main.humidity.ToString() + "%";
+
+                Text_Wind.Content = oW.wind.deg.ToString() + "°";
+
+                Text_Direction.Content = oW.wind.speed.ToString() + "м/с";
+
+                IconWeather.Source = oW.weather[0].Icon;
+
+                GridWeather.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                GridWeather.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private bool CheckNetwork()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                return false;
+            }
+
+            bool isConnected = false;
+            using (var tcpClient = new TcpClient())
+            {
+                tcpClient.Connect("209.85.148.138", 443); // google
+                isConnected = tcpClient.Connected;
+                return true;
+            }
+        }
+
+        private void SpecialtiesClick(object sender, RoutedEventArgs e)
+        {
+            Specialties specialties = new Specialties();
+            specialties.ShowDialog();
+        }
+
+        private void Workshops_Click(object sender, RoutedEventArgs e)
+        {
+            Workshops workshops = new Workshops();
+            workshops.ShowDialog();
         }
     }
 }
