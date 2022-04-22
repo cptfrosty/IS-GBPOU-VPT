c86d5358fe87109c19dfe96ee94443a6d9ecd13d
Twinkle <dim.iwanencko2014@gmail.com>
2022-04-22 12:41:05 +0400

    Update

--- Terminal/Terminal/WindowInf.xaml.cs
+++ Terminal/Terminal/WindowInf.xaml.cs
@@ -19,79 +19,22 @@ namespace Terminal
     /// </summary>
     public partial class WindowInf : Window
     {
-        public int countCorp;
-        
         public WindowInf()
         {
             InitializeComponent();
-            Init();
         }
-        // Кнопка выхода
-        private void Exit(object sender, RoutedEventArgs e)
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-        // Создание кнопок
-        private void Init()
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            XmlCollegeBuilding xmlCorps = new XmlCollegeBuilding();
-            int count = XmlCollegeBuilding.Instance().GetCountCorp;
-
-            for (int i = 1; i <= count; i++)
-            {
-                Button btn = new Button();               
-                btn.Width = 211;
-                btn.Height = 160;
-                btn.Margin = new Thickness(15, 100, 15, 15);
-                
-                btn.Name = "id" + i;
-
-                btn.Click += CollegeBuildingInformation;
-
-                Label label = new Label();
-                label.Height = 40;
-                label.Margin = new Thickness(0, 130, 0, 0);
-                label.Content = $"Корпус {i}";  
-                DockPanel dp = new DockPanel();
-                dp.LastChildFill = true;
-                DockPanel.SetDock(label, Dock.Bottom);
-                dp.Children.Add(label);
-               
-
-                BitmapImage bm = new BitmapImage();
-                bm.BeginInit();
-                bm.UriSource = new Uri("Icon/logo2.png", UriKind.Relative);
-                bm.EndInit();
-
-                
-                btn.BorderBrush = Brushes.Black;
-                btn.BorderThickness = new Thickness(2);
-                
-
-                ImageBrush imageBrush = new ImageBrush(bm);
-                imageBrush.Stretch = Stretch.UniformToFill;
-                btn.Background = imageBrush;
-
-                btn.Content = dp;
-
-                panelKorpus.Children.Add(btn);
-            }
-        }
-        // Задержка при клике
-        private void CollegeBuildingInformation(object sender, RoutedEventArgs e)
-        {
-            string nameButton = ((Button)sender).Name;
-
-            _ = MethodWithDelayAsync(nameButton, 400);
-        }
-        public async Task MethodWithDelayAsync(string btnName,int milliseconds)
-        {
-            await Task.Delay(milliseconds);
-
-            btnName = btnName.Replace("id", "");
-            int choice = int.Parse(btnName);
-
+            int choice = 1;
+            
             FrameForm frameForm = new FrameForm(choice);
+            
             frameForm.ShowDialog();
         }
     }
