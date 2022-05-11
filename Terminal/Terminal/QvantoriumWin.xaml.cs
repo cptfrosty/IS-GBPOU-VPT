2ff3388b7582c28e048c474bad01ab7a5dd986a6
Denchick <danilremeskov@yandex.ru>
2022-05-11 09:53:52 +0400

    update Qv

--- Terminal/Terminal/QvantoriumWin.xaml.cs
+++ Terminal/Terminal/QvantoriumWin.xaml.cs
@@ -24,6 +24,7 @@ namespace Terminal
         {
             InitializeComponent();
             PlaceInfo();
+          
         }
 
         private void Exit(object sender, RoutedEventArgs e)
@@ -36,10 +37,69 @@ namespace Terminal
             XDocument xdoc = XDocument.Load(AppDomain.CurrentDomain.BaseDirectory + "Frame/Qvantorium.xml");
             foreach (XElement info in xdoc.Element("informations").Elements("info"))
             {                                                                    
-                atrText.Text = info.Attribute("name").Value;
+                atrText.Content = info.Attribute("name").Value;
                 adressText.Text = info.Element("adress").Value;
                 desText.Text = info.Element("des").Value;
             }
+        }
+
+        //TEST
+        private System.Windows.Point scrollTarget;
+        private System.Windows.Point scrollStartPoint;
+        private System.Windows.Point scrollStartOffset;
+      
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            if (scrollViewer.IsMouseOver)
+            {
+                // Save starting point, used later when determining how much to scroll.
+                scrollStartPoint = e.GetPosition(this);
+                scrollStartOffset.X = scrollViewer.HorizontalOffset;
+                scrollStartOffset.Y = scrollViewer.VerticalOffset;
+
+                // Update the cursor if can scroll or not.
+                this.Cursor = (scrollViewer.ExtentWidth > scrollViewer.ViewportWidth) ||
+                    (scrollViewer.ExtentHeight > scrollViewer.ViewportHeight) ?
+                    Cursors.ScrollAll : Cursors.Arrow;
+
+                this.CaptureMouse();
+            }
+
+            base.OnPreviewMouseDown(e);
+        }
+
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            if (this.IsMouseCaptured)
+            {
+                System.Windows.Point currentPoint = e.GetPosition(this);
+
+                // Determine the new amount to scroll.
+                System.Windows.Point delta = new System.Windows.Point(scrollStartPoint.X -
+                    currentPoint.X, scrollStartPoint.Y - currentPoint.Y);
+
+                scrollTarget.X = scrollStartOffset.X + delta.X;
+                scrollTarget.Y = scrollStartOffset.Y + delta.Y;
+
+                // Scroll to the new position.
+                scrollViewer.ScrollToHorizontalOffset(scrollTarget.X);
+                scrollViewer.ScrollToVerticalOffset(scrollTarget.Y);
+            }
+
+            base.OnPreviewMouseMove(e);
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            if (this.IsMouseCaptured)
+            {
+                this.Cursor = Cursors.Arrow;
+                this.ReleaseMouseCapture();
+            }
+
+            base.OnPreviewMouseUp(e);
         }
     }
 }
