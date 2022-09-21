e1e3b059f7b823b0788e44ccfe5549fc4ec16094
Twinkle <dim.iwanencko2014@gmail.com>
2022-09-21 23:51:02 +0300

    remote connection

---
+++ Terminal/Terminal/Resources/Server/Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketTcpServer
+{
+    class Program
+    {
+        static int port = 8005; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+
+            // Получение имени компьютера.
+            String host = System.Net.Dns.GetHostName();
+            // Получение ip-адреса.
+            System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[1];
+            // Показ адреса в label'е.
+            string IPstring = ip.ToString();
+
+            Console.WriteLine($"{IPstring}");
+
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse($"{IPstring}"), port);
+
+            // создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+
+                // начинаем прослушивание
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    switch (builder.ToString())
+                    {
+                        case "1":
+                            var proc = new System.Diagnostics.Process();
+                            proc.StartInfo.FileName = "C:\\Windows\\explorer.exe";
+                            proc.StartInfo.UseShellExecute = true;
+                            proc.Start();
+                            break;
+                    }
+
+                    // отправляем ответ
+                    string message = "Выполнено!";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+                    // закрываем сокет
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
